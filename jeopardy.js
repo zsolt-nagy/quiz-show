73decc37a337ce8f26e5daba1551ba47b6074d9f
Zsolt Nagy <info@zsoltnagy.eu>
2023-06-10 01:58:41 +0200

    display selected question with timer (WIP)

--- jeopardy.js
+++ jeopardy.js
@@ -1,10 +1,29 @@
 let board = document.querySelector(".js-board");
+let questionDisplayNode = document.querySelector(".js-question-display");
+let questionTextNode = document.querySelector(".js-question-text");
+let questionTimeLeftNode = document.querySelector(".js-question-time");
 
 let categoryList = null;
 let questionLists = null;
 
 let blockNewQuestions = false;
+let questionStartTimestamp = null;
+let questionTimerId = null;
 
+// Initialization
+function initialize() {
+    // get categories
+    fetch("https://opentdb.com/api_category.php")
+        .then((x) => x.json())
+        .then(categoriesLoaded);
+}
+
+function testInitialize() {
+    questionLists = testQuestions;
+    renderBoard();
+}
+
+// Rendering and retrieval of questions
 function renderQuestions(questionsList, columnIndex) {
     let html = "";
     for (let i = 0; i < questionsList.length; i++) {
@@ -69,21 +88,8 @@ function categoriesLoaded(categories) {
     retrieveQuestions(selectedCategories);
 }
 
-function initialize() {
-    // get categories
-    fetch("https://opentdb.com/api_category.php")
-        .then((x) => x.json())
-        .then(categoriesLoaded);
-}
-
-function testInitialize() {
-    questionLists = testQuestions;
-    renderBoard();
-}
-
-function boardClicked(event) {
-    if (blockNewQuestions) return;
-
+// Event handling - gameplay
+function getTargetNodeFromEvent(event) {
     // navigate to the target node with data-row, data-column attributes
     let targetNode = event.target;
     if (targetNode.classList.contains("js-question")) {
@@ -97,22 +103,55 @@ function boardClicked(event) {
         targetNode = null;
     }
 
+    return targetNode;
+}
+
+function questionTick() {
+    let currentTimestamp = new Date().getTime();
+    let timeElapsed = currentTimestamp - questionStartTimestamp;
+    let numberOfDots = Math.max(0, Math.round(((10000 - timeElapsed) / 10000) * 20));
+    let dots = new Array(numberOfDots).fill("•").join("");
+    questionTimeLeftNode.innerHTML = dots;
+
+    if (numberOfDots === 0) {
+        clearInterval(questionTimerId);
+        questionDisplayNode.classList.add("invisible");
+        // TODO clean up
+        blockNewQuestions = false;
+    }
+}
+
+function showQuestion(question) {
+    questionTextNode.innerHTML = question.question;
+    questionDisplayNode.classList.remove("invisible");
+    questionStartTimestamp = new Date().getTime();
+    questionTimerId = setInterval(questionTick, 100);
+}
+
+function selectQuestion(targetNode) {
+    let rowIndex = targetNode.dataset.row;
+    let columnIndex = targetNode.dataset.column;
+
+    targetNode.classList.remove("js-face-value");
+    targetNode.classList.remove("face-value");
+    targetNode.classList.add("js-question-done");
+    targetNode.innerHTML = "?";
+
+    blockNewQuestions = true;
+
+    setTimeout(() => {
+        targetNode.innerHTML = "";
+        showQuestion(questionLists[columnIndex][rowIndex]);
+    }, 1000);
+}
+
+function boardClicked(event) {
+    if (blockNewQuestions) return;
+
+    let targetNode = getTargetNodeFromEvent(event);
+
     if (targetNode !== null) {
-        let rowIndex = targetNode.dataset.row;
-        let columnIndex = targetNode.dataset.column;
-
-        targetNode.classList.remove("js-face-value");
-        targetNode.classList.remove("face-value");
-        targetNode.classList.add("js-question-done");
-        targetNode.innerHTML = "?";
-
-        blockNewQuestions = true;
-
-        setTimeout(() => {
-            targetNode.innerHTML = "";
-            console.log(questionLists[columnIndex][rowIndex]);
-            blockNewQuestions = false;
-        }, 1000);
+        selectQuestion(targetNode);
     }
 }
 
